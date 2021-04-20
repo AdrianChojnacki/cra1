8c32d4868d5fb79a33ba2ed2280f8ccf285fa88b
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-04-20 21:23:27 +0200

    stoper

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,39 @@
 import React, { Component } from "react";
 import "./App.css";
+import SwitchButton from "./SwitchButton";
 
-export default class App extends Component {
+class App extends Component {
+  state = {
+    time: 0,
+    active: false,
+  };
+
+  addSecond = () => {
+    this.setState(() => ({
+      time: this.state.time + 1,
+    }));
+  };
+
+  handleClick = () => {
+    if (this.state.active) {
+      clearInterval(this.idInterval);
+    } else {
+      this.idInterval = setInterval(() => this.addSecond(), 1000);
+    }
+
+    this.setState({
+      active: !this.state.active,
+    });
+  };
+
   render() {
     return (
       <>
-        <div className="app">Aplikacja</div>
+        <p>{this.state.time}</p>
+        <SwitchButton click={this.handleClick} active={this.state.active} />
       </>
     );
   }
 }
+
+export default App;
